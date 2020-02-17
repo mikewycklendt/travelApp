7398c4460eeb95242a5cbddc2e05477c102b9c7e
Mike <mikewycklendt@gmail.com>
2020-02-17 08:59:24 -0600

    imported scss

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,6 +1,10 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/style.scss'
+
 console.log(checkForName);
 
 alert("I EXIST")
